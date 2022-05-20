05d9aff786e4a75d9ef217e39bb8e9147d0acaab
George Evans <george@gravitywell.co.uk>
2022-05-20 15:26:10 +0100

    Initial commit

---
+++ test/example-lambda-to-ecs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ExampleLambdaToEcs from '../lib/example-lambda-to-ecs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example-lambda-to-ecs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ExampleLambdaToEcs.ExampleLambdaToEcsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
